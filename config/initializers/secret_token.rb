e24449a33a674be213aa9dc8113655c66aca2dbe
bennux <bennux@gmail.com>
2013-06-27 21:37:06 +0000

    initial commit from nitrous.io box

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '6e74e81078808a27ff8075d9e88ab58f177fdb5c2ea080978a86fb379a7e4eafdc7dcf88f65ab3a4245591180d227e68991c827da7e8a1b5c7c0146959151499'
